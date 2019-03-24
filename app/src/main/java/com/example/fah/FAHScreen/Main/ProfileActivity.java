44d6a3a080a910a35f776300de6ea9b48e2722b5
Công Thành <you@example.com>
2019-03-24 19:58:47 +0700

    ThanhDC11

---
+++ app/src/main/java/com/example/fah/FAHScreen/Main/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.fah.Main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.fah.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+    }
+}
