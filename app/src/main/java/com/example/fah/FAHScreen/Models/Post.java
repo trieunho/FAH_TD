98517a5d84b1b258c77bb6e745d67919943120d3
đặng cảnh <avancanh@gmail.com>
2019-03-24 14:50:58 +0700

    Mys

---
+++ app/src/main/java/com/example/fah/FAHScreen/Models/Post.java
@@ -0,0 +1,37 @@
+package com.example.fah.Model;
+
+public class Post {
+    private String titlePost;
+    private String account;
+    private String companyName;
+
+    public Post(String titlePost, String account, String companyName) {
+        this.titlePost = titlePost;
+        this.account = account;
+        this.companyName = companyName;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getTitlePost() {
+        return titlePost;
+    }
+
+    public void setTitlePost(String titlePost) {
+        this.titlePost = titlePost;
+    }
+}
