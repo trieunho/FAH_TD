87cf21640a7ccf0723c2363c235d60ff5ed94fa0
trieunho <trieunhoa2@gmail.com>
2019-04-07 00:08:28 +0700

    add Query firebase

---
+++ app/src/main/java/com/example/fah/FAHDatabase/Table/Account.java
@@ -0,0 +1,47 @@
+package com.example.fah.FAHDatabase.Table;
+
+public class Account {
+    private String name,email,key;
+    private int resourceImg;
+
+    public Account() {
+    }
+
+    public Account(String name, String email, int resourceImg) {
+        this.name = name;
+        this.email = email;
+        this.resourceImg = resourceImg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getResourceImg() {
+        return resourceImg;
+    }
+
+    public void setResourceImg(int resourceImg) {
+        this.resourceImg = resourceImg;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
