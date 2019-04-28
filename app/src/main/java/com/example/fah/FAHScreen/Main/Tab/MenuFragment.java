09a7fb3aafb5f52c2d141b4e55469630eb2a021a
Công Thành <you@example.com>
2019-04-28 15:18:43 +0700

    commit source ThanhDC11

--- app/src/main/java/com/example/fah/FAHScreen/Main/Tab/MenuFragment.java
+++ app/src/main/java/com/example/fah/FAHScreen/Main/Tab/MenuFragment.java
@@ -57,6 +57,9 @@ public class MenuFragment extends Fragment {
                                     MainActivity.userLogin.setAvata(img.getSource());
                                     progressDoalog.dismiss();
                                     GetControl();
+                                }else{
+                                    progressDoalog.dismiss();
+                                    GetControl();
                                 }
                             }
 
