985ee35db21fa041d8b8f04760027626bf4c8dc6
trieunho <trieunhoa2@gmail.com>
2019-05-16 00:27:00 +0700

    Fix notification

--- app/src/main/java/com/example/fah/FAHScreen/Main/Tab/NotificationFragment.java
+++ app/src/main/java/com/example/fah/FAHScreen/Main/Tab/NotificationFragment.java
@@ -1,6 +1,7 @@
 package com.example.fah.FAHScreen.Main.Tab;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +10,21 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.fah.FAHCommon.FAHDatabase.FAHQuery;
+import com.example.fah.FAHCommon.FAHDatabase.Table.FAHQueryParam;
+import com.example.fah.FAHCommon.FAHDatabase.Table.TestDB;
+import com.example.fah.FAHData.AccountData;
 import com.example.fah.FAHData.NotificationData;
 import com.example.fah.FAHModel.Adapters.NotificationAdapter;
 import com.example.fah.FAHModel.Models.IEventData;
 import com.example.fah.FAHModel.Models.Notification;
 import com.example.fah.R;
+import com.example.fah.TestControl.Sample_Add_Edit_Delete_Account;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * create an instance of this fragment.
@@ -37,26 +48,26 @@ public class NotificationFragment extends Fragment {
     }
 
     private void GridControl(){
-        NotificationData.setUpNotificationData(new IEventData() {
-            @Override
-            public void EventSuccess() {
-                final ListView listView = view.findViewById(R.id.notificationList);
-                listView.setAdapter(new NotificationAdapter(getContext(), R.layout.list_notification_item, NotificationData.listNotifications));
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        final ListView listView = view.findViewById(R.id.notificationList);
+        if (AccountData.userLogin != null) {
+            if (AccountData.userLogin.getKey() != null) {
+                //load data user
+                FAHQueryParam queryParam = new FAHQueryParam("Notification", "accountKey", FAHQueryParam.EQUAL, AccountData.userLogin.getKey(), FAHQueryParam.TypeString);
+
+                FAHQuery.GetDataQuery(queryParam).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        ArrayList<Notification> listNotifications = (ArrayList<Notification>) FAHQuery.GetDataObject(dataSnapshot, new Notification());
+                        listView.setAdapter(new NotificationAdapter(getContext(), R.layout.list_notification_item, listNotifications));
+                    }
 
                     @Override
-                    public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-                        Notification notification = NotificationData.listNotifications.get(position);
-                        Toast.makeText(getContext(), notification.getBody(), Toast.LENGTH_SHORT).show();
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Toast.makeText(getContext(), "Lỗi", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
-
-            @Override
-            public void EventFail(String message) {
-                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        }
     }
 }
